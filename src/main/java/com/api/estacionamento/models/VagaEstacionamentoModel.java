22b46fdef43fedc9c7d0476725719fd44804176a
manzanofp <felipemanzanofp@gmail.com>
2022-02-09 12:51:12 -0300

    Projeto criado

---
+++ src/main/java/com/api/estacionamento/models/VagaEstacionamentoModel.java
@@ -0,0 +1,10 @@
+package com.api.estacionamento.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_Vaga_Estacionamento")
+public class VagaEstacionamentoModel {
+    
+}
